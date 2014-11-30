f1a517fabf4e0e112010b9311549b500c5292872
Erik Price <erik.price16@gmail.com>
2014-11-30 18:26:41 -0500

    Additional stubs to CryptoUtil

--- frontend/js/util/CryptoUtil.js
+++ frontend/js/util/CryptoUtil.js
@@ -15,16 +15,28 @@ module.exports = {
     return array;
   },
 
-  _encrypt: function(key, msg) {
+  _encrypt: function(key, msgBytes) {
+    var cipher = forge.cipher.createCipher('AES-GCM', key);
     var iv = this.genRandomBytes(16);
+
+    cipher.start({
+      iv: iv,
+      tagLength: 128
+    });
+
+    cipher.update(forge.util.createBuffer(msgBytes));
+    cipher.finish();
+
+    var encrypted = cipher.output;
+    var tag = cipher.mode.tag;
 
     // TODO: this
   },
 
-  _decrypt: function(key, msg) {
-    var iv = msg.slice(0, 16);
-    var tag = msg.slice(16, 32);
-    var encrypted = msg.slice(32);
+  _decrypt: function(key, msgBytes) {
+    var iv = msgBytes.slice(0, 16);
+    var tag = msgBytes.slice(16, 32);
+    var encrypted = msgBytes.slice(32);
 
     var decipher = forge.cipher.createDecipher('AES-GCM', key);
 
